fac2667655672cc9c1cf6926a0100c5c9ef17160
doctor <nikitalystsev@yandex.ru>
2023-10-04 13:08:20 +0300

    добавил отслочек на доктора кто в диагнозы

--- db/labs/lab_01/config.py
+++ db/labs/lab_01/config.py
@@ -6,7 +6,7 @@ class BaseData:
 
     def __init__(self, file_department: str):
         """Инициализация"""
-        self.__speciality = list()
+        self.__data = list()
 
         with open(file_department) as file:
             self.__speciality = file.readlines()
