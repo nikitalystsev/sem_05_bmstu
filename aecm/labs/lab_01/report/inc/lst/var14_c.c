dbc2b459ad05c69e83696db574181bf460624f99
doctor <nikitalystsev@yandex.ru>
2023-10-17 03:49:24 +0300

    наконец таки сделал первую лабу

---
+++ aecm/labs/lab_01/report/inc/lst/var14_c.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define len 9
+#define enroll 1
+#define elem_sz 4
+
+int _x[] = {1, 2, 3, 4, 8, 7, 6, 5, 4};
+
+int main(void) {
+  int *x1 = _x;
+  int *x20 = x1 + len + 1;
+  int x31 = x1[0];
+
+  x1 += 1;
+
+  do {
+    int x2 = x1[0];
+    if (x2 >= x31) {
+      x31 = x2;
+    }
+    x1 += enroll;
+  } while (x1 != x20);
+
+  printf("x31 = %d\n", x31);
+
+  while (1) {
+  }
+
+  return 0;
+}
