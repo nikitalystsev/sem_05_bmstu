78deedb37ed7d00e993b0c0d2f96c594f9e66f07
doctor <nikitalystsev@yandex.ru>
2023-12-07 13:44:10 +0300

    сделал  первую задачу 5-й лабы

---
+++ db/labs/lab_05/src/task1.sql
@@ -0,0 +1,43 @@
+
+-- Из таблиц базы данных, созданной в первой лабораторной работе, извлечь
+-- данные в XML (MSSQL) или JSON(Oracle, Postgres). Для выгрузки в XML
+-- проверить все режимы конструкции FOR XML
+
+-- sudo chown postgres:postgres <имя файла>.json - смена владельца файла на postgres
+-- по другому не зааписывает, почему то
+
+-- копируем все admissions в json файл
+copy 
+(
+	select row_to_json(adm) 
+	from admissions as adm 
+) 
+to '/var/lib/postgres/data/admissions.json';
+
+copy 
+(
+	select row_to_json(doc) 
+	from doctors as doc
+) 
+to '/var/lib/postgres/data/doctors.json';
+
+copy 
+(
+	select row_to_json(pat) 
+	from patients as pat
+) 
+to '/var/lib/postgres/data/patients.json';
+
+copy 
+(
+	select row_to_json(dia) 
+	from diagnoses as dia
+) 
+to '/var/lib/postgres/data/diagnoses.json';
+
+copy 
+(
+	select row_to_json(w) 
+	from wards as w
+) 
+to '/var/lib/postgres/data/wards.json';
