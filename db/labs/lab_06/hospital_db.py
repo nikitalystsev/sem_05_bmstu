65f0c3a70939383d0dd18b67e773a22624842c7b
doctor <nikitalystsev@yandex.ru>
2023-12-15 20:51:20 +0300

    сделал 6-ю лабу

--- db/labs/lab_06/hospital_db.py
+++ db/labs/lab_06/hospital_db.py
@@ -75,7 +75,191 @@ class HospitalDB:
                     """
                 )
 
-                print(cursor.fetchone())
+                for row in cursor:
+                    print(row)
 
         except Exception as _ex:
             print("[INFO] Ошибка выполнения запроса с несколькими join", _ex)
+
+    def cte_row_number_query(self):
+        """
+        Метод реализует запрос с ОТВ(CTE) и оконными функциями
+        Устранение полных дублей с помощью оконной функции row_number
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    with dupl as
+                    ( 
+                        select *
+                        from admissions
+                        
+                        union all
+                        
+                        select *
+                        from admissions
+                    ), del_dupl as
+                    (
+                        select d.id, 
+                            row_number() over (partition by d.id) as row_num
+                        from dupl as d
+                    )
+                    
+                    select *
+                    from del_dupl
+                    where row_num = 1;
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения запроса с ОТВ(CTE) и оконными функциями", _ex)
+
+    def metadata_query(self):
+        """
+        Метод реализует запрос к метаданным
+        Вывести данные об типах и атрибутах таблицы patients
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT column_name, data_type
+                    FROM information_schema.columns
+                    WHERE table_name = 'patients';
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения запроса к метаданным", _ex)
+
+    def call_scalar_func_query(self):
+        """
+        Метод реализует вызов скалярной функции (написанной в третьей лабораторной работе);
+        Вывести количество пациентов у конкретного доктора
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    select count_patients_by_doc('02bc8b61-b4eb-4227-adf1-031ce04d8a6d'); -- 3
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова скалярной функции", _ex)
+
+    def call_multi_statement_or_table_query(self):
+        """
+        Метод реализует вызов многооператорной или табличной функции (написанной в третьей лабораторной работе);
+        вывести более читаемые записи о поступлениях с диагнозами с вероятностью смерти менее переданного
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    select * 
+                    from best_read_adm_less_death(70);
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова многооператорной или табличной функции", _ex)
+
+    def call_stored_procedure_query(self):
+        """
+        Метод реализует вызов хранимой процедуры (написанной в третьей лабораторной работе);
+        повысить зарплату всем врачам переданной в качестве параметра должности
+        на 20 процентов от средней зарплате по этой же должности
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    call increase_wage_by_position('Диабетолог');
+                    
+                    select doc.fio, doc.position, doc.wage
+                    from doctors as doc
+                    where position = 'Диабетолог'
+                    order by doc.wage;
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова хранимой процедуры", _ex)
+
+    def call_system_procedure_query(self):
+        """
+        Метод реализует вызов системной процедуры;
+        Получить версию сервера Postgresql
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    select version();
+                    """
+                )
+
+                for row in cursor:
+                    print(row)
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова хранимой процедуры", _ex)
+
+    def create_table_query(self):
+        """
+        Метод реализует запрос создания новой таблицы, соответствующей тематике БД
+        Создать таблицу с лекарствами
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    create table if not exists medicines (
+                        id uuid,
+                        name varchar(20),
+                        price integer
+                    );
+                    """
+                )
+
+                print("[INFO] Таблица с лекарствами была успешно создана")
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова хранимой процедуры", _ex)
+
+    def insert_medicines_query(self):
+        """
+        Метод реализует вставку данных в созданную таблицу с использованием инструкции INSERT или COPY
+
+        """
+        try:
+            with self.__connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    insert into medicines (name, price) 
+                    values ('глицин', 100),
+                            ('амброксол', 400)
+                    """
+                )
+
+                print("[INFO] Данные в таблицу с лекарствами были успешно добавлены")
+
+        except Exception as _ex:
+            print("[INFO] Ошибка выполнения вызова хранимой процедуры", _ex)
--- db/labs/lab_06/main.py
+++ db/labs/lab_06/main.py
@@ -13,7 +13,16 @@ def main() -> None:
         db_name=db_name
     )
 
-    hospital_db.scalar_query()
+    # hospital_db.scalar_query()
+    # hospital_db.with_several_join_query()
+    # hospital_db.cte_row_number_query()
+    # hospital_db.metadata_query()
+    # hospital_db.call_scalar_func_query()
+    # hospital_db.call_multi_statement_or_table_query()
+    # hospital_db.call_stored_procedure_query()
+    # hospital_db.call_system_procedure_query()
+    # hospital_db.create_table_query()
+    # hospital_db.insert_medicines_query()
 
 
 if __name__ == "__main__":
