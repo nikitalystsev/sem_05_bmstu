5752537dd1a1a23910ddcaa0b842aba33b45ed0b
nikitalystsev <nikitalystsev@yandex.ru>
2024-01-31 10:26:07 +0300

    приготовил файлы для реализации конвейера

---
+++ aa/lab_05/inc/conveyor.h
@@ -0,0 +1,14 @@
+#ifndef __CONVEYOR_H__
+#define __CONVEYOR_H__
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <pthread.h>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#endif
---
+++ aa/lab_05/inc/serial.h
@@ -0,0 +1,14 @@
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <pthread.h>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#endif
