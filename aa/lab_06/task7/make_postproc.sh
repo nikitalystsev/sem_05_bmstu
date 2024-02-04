0592779169e6f966973e0671c6e2a6dd4467915b
nikitalystsev <nikitalystsev@yandex.ru>
2024-02-04 15:31:54 +0300

    сделал лабу

--- aa/lab_06/task7/make_postproc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd postproc_scripts || exit
-
-./create_linear_time1.gpi
-# ./create_linear_time2.gpi
-# ./create_linear_mem.gpi
-
-cd ..
--- aa/lab_06/task7/preproc_scripts/build_scripts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++20 -Wall -Werror -Wpedantic -Wextra -Wfloat-conversion -Wfloat-equal -Wvla -o ar_mean.exe ar_mean.c -lm
